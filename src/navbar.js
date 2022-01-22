21cbc7471c6ac65b533450a3e351354e63bddcda
AnikaMahesh <52103193+AnikaMahesh@users.noreply.github.com>
2022-01-22 15:37:10 -0800

    null

---
+++ src/navbar.js
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return <div>
+        <h1>beautiful sunsets</h1>
+        <img src="https://www.freepsdbazaar.com/wp-content/uploads/2020/06/sky-replace/sky-sunset/sunset-049-freepsdbazaar.jpg"></img>
+                        
+    </div>
+}
